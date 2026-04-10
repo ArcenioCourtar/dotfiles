31e4dcfb06d64b2ebda710bba7fe024b22ecc839
Arcenio Courtar <71286201+ArcenioCourtar@users.noreply.github.com>
2026-04-10 17:00:15 +0200

    got the basics of the lsp going

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,4 +1,52 @@
 return {
-    "mason-org/mason.nvim",
-    opts = {}
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			{ "williamboman/mason.nvim", opts = {}, },
+			{ "neovim/nvim-lspconfig",
+				dependencies = {
+					{ "folke/lazydev.nvim",
+						ft = "lua",
+						opts = {
+							library = {
+								{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+							},
+						},
+					},
+					{ 'saghen/blink.cmp' },
+				},
+				opts = {
+					servers = {
+						lua_ls = {}, -- lua
+						clangd = {}, -- c/cpp
+						nil_ls = {}, -- nix
+						bashls = {}, -- sh/bash
+						htmlhint = {}, -- html
+						css = {}, -- css
+						hls = {},
+					},
+				},
+				config = function(_, opts)
+					for server, config in pairs(opts.servers) do
+						config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+						vim.lsp.enable(server)
+					end
+				end,
+			}
+		},
+		opts = {
+			ensure_installed = {
+				lua_ls = {}, -- lua
+				clangd = {}, -- c/cpp
+				nil_ls = {}, -- nix
+				bashls = {}, -- sh/bash
+				htmlhint = {}, -- html
+				css = {}, -- css
+				hls = {},
+			}
+		},
+	},
 }
+
+-- I cannot get mason and treesitter to automatically install parsers/lsps,
+-- for :MasonInstall to install the necessary LSPs 
--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -3,3 +3,6 @@ return {
   lazy = false,
   build = ':TSUpdate'
 }
+
+-- I cannot get autoinstall to work for neither TS or Mason,
+-- Manually install parsers using :TSUpdate [language] in nvim
